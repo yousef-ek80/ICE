2f63d3cbf02d9eb7e8775ca37a335f5a4401869f
yousef El-khadray <elkhadra@sheridancollege.ca>
2021-10-06 20:44:11 -0400

    second Commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,13 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Yousef El-Khadray
+ * Student number: 99162014
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card newObj = new Card();
     }
     
 }
